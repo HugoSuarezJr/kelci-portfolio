dfa96530cda959e14379900b525a0a408df26713
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-03-09 10:31:55 -0500

    Create routes and views

---
+++ Desktop/Laravel-Blog/resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<script src="/app.js"></script>
+
+<body>
+<article>
+        <h1><a href='/post'>My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti explicabo magnam asperiores atque ab minus odit expedita facere similique voluptatem eligendi laborum amet ipsum, veniam quod cum vero nisi.</p>
+    </article>
+
+    <a href="/"><- Go Back</a>
+</body>
